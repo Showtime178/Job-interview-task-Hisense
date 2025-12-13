cce62f6e014c4175c00b7e07eabd188d82688d0f
Showtime-Mitja <Showtime-Mitja@DESKTOP-MC3VLHI>
2025-12-13 10:22:41 +0100

    Add project files.

---
+++ Infrastructure/Data/InMemoryDatabase.cs
@@ -0,0 +1,16 @@
+﻿using Infrastructure.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Infrastructure.Data
+{
+    public static class InMemoryDatabase
+    {
+        public static readonly List<Product> Products = new()
+        {
+            new Product { Code = "RB5K330GSFD", Model = "Samostojni hladilnik z zamrzovalnikom", Height = 1856, Width = 595, Depth = 600 },
+            new Product { Code = "RQ760N4IFE", Model = "Hi9 Samostojni štirivratni hladilnik", Height = 1785, Width = 914, Depth = 725 }
+        };
+    }
+}
---
+++ Infrastructure/Models/Product.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Infrastructure.Models
+{
+    public class Product
+    {
+        //Šifra izdelka
+        public string Code { get; set; } = string.Empty;
+
+        //Model izdelka
+        public string Model { get; set; } = string.Empty;
+
+        //Širina aparata
+        public int Width { get; set; }
+
+        //Višina aparata
+        public int Height { get; set; }
+        
+        //Globina aparata
+        public int Depth { get; set; }
+    }
+}
