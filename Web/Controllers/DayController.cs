25a9f3eed30ba04bd6f6ee295c3ae2814b5900b1
Showtime-Mitja <mitja2.stefanec@gmail.com>
2025-12-14 14:56:41 +0100

    -Dodal REST endpoint kateri sprejme celo število in vrne ime dneva v tednu v angleščini in Swagger UI ter povezavo na Swagger UI

---
+++ Web/Controllers/DayController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Web.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class DayController : ControllerBase
+    {
+        [HttpGet("{dayNumber}")]
+        public IActionResult GetDay(int dayNumber)
+        {
+            var days = new[] { "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday" };
+            if (dayNumber < 1 || dayNumber > 7)
+                return BadRequest("Napačna številka dneva, mora bit me 1 in 7.");
+
+            return Ok(days[dayNumber - 1]);
+        }
+    }
+}
--- Web/Program.cs
+++ Web/Program.cs
@@ -1,5 +1,6 @@
-using Infrastructure.Models;
+﻿using Infrastructure.Models;
 using Infrastructure.Repositories;
+using Microsoft.OpenApi;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -7,9 +8,32 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews(); 
 builder.Services.AddRazorPages();
 builder.Services.AddSingleton<IRepository<Product>, ProductRepository>();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddControllers();
+
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "API za dneve v tednu",
+        Version = "v1",
+        Description = "API kateri trenutno vrača samo dneve v tednu v angleščini."
+    });
+});
 
 var app = builder.Build();
 app.MapRazorPages();
+app.MapControllers();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "API za dneve v tednu");
+    });
+}
 
 app.UseHttpsRedirection();
 app.UseRouting();
